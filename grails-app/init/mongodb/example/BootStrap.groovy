1c45913ea0386d1a1c9b52878e5663d988b7bb10
Abhishek Taparia <abhishek@wizpanda.com>
2021-04-07 19:09:20 +0530

    Added product on bootstrap

--- grails-app/init/mongodb/example/BootStrap.groovy
+++ grails-app/init/mongodb/example/BootStrap.groovy
@@ -3,6 +3,11 @@ package mongodb.example
 class BootStrap {
 
     def init = { servletContext ->
+        Product product = new Product();
+        product.name = "Apple"
+        product.price = 20
+
+        product.save(flush: true, failOnError: true)
     }
     def destroy = {
     }
